c02875aa36b98f61db085924f42ea26dcc1ce761
K <kihyuk.sung@outlook.kr>
2023-02-17 19:56:09 +0900

    feat: first commit
    - spring webflux setting

---
+++ src/test/kotlin/org/k/shortener/ShortenerApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.k.shortener
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShortenerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
