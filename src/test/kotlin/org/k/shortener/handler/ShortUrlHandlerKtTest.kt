2bf56bce70b0cb2fba79e72315f3636ff8717662
K <kihyuk.sung@outlook.kr>
2023-02-18 21:03:02 +0900

    test: post url
    - URL 등록
    - body는 url: String 형식
    - 일단 응답은 echo

--- src/test/kotlin/org/k/shortener/handler/ShortUrlHandlerKtTest.kt
+++ src/test/kotlin/org/k/shortener/handler/ShortUrlHandlerKtTest.kt
@@ -4,11 +4,13 @@ import io.kotest.core.extensions.Extension
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.extensions.spring.SpringExtension
 import io.kotest.matchers.shouldBe
+import org.k.shortener.config.CreateShortUrlDto
 import org.k.shortener.config.RouterConfiguration
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
 import org.springframework.http.MediaType
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.reactive.server.expectBody
 
 @WebFluxTest
 @ContextConfiguration(classes = [RouterConfiguration::class])
@@ -24,6 +26,18 @@ internal class ShortUrlHandlerKtTest(private val webClient: WebTestClient) : Fun
                 .expectBody(String::class.java)
                 .value {
                     it shouldBe "helloWorld"
+                }
+        }
+
+        test("post url") {
+            webClient.post()
+                .uri("/url")
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(CreateShortUrlDto("http://www.example.com"))
+                .exchange()
+                .expectBody<CreateShortUrlDto>()
+                .value {
+                    it shouldBe CreateShortUrlDto("http://www.example.com")
                 }
         }
     }
