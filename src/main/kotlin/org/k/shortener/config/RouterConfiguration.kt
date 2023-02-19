ac9dce776a6e2010c1a261c414bb93c6102104b2
K <kihyuk.sung@outlook.kr>
2023-02-19 15:07:05 +0900

    feat: handleCreateShortUrl로 분리
    - 여기서는 body parse, validate 등 수행

--- src/main/kotlin/org/k/shortener/config/RouterConfiguration.kt
+++ src/main/kotlin/org/k/shortener/config/RouterConfiguration.kt
@@ -1,12 +1,10 @@
 package org.k.shortener.config
 
-import kotlinx.coroutines.reactor.awaitSingle
+import org.k.shortener.handler.handleCreateShortUrl
 import org.k.shortener.handler.makeShortUrlHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType
-import org.springframework.web.reactive.function.server.ServerResponse
-import org.springframework.web.reactive.function.server.awaitBody
 import org.springframework.web.reactive.function.server.coRouter
 
 @Configuration
@@ -15,15 +13,7 @@ class RouterConfiguration {
     fun mainRouter() = coRouter {
         GET("/{shortUrl}", makeShortUrlHandler("shortUrl"))
         accept(MediaType.APPLICATION_JSON).nest {
-            POST("/url") { request ->
-                request.awaitBody<CreateShortUrlDto>()
-                    .let {
-                        ServerResponse
-                            .ok()
-                            .bodyValue(it)
-                            .awaitSingle()
-                    }
-            }
+            POST("/url", ::handleCreateShortUrl)
         }
     }
 }
---
+++ src/main/kotlin/org/k/shortener/handler/CreateShortUrlHandler.kt
@@ -0,0 +1,19 @@
+package org.k.shortener.handler
+
+import kotlinx.coroutines.reactor.awaitSingle
+import org.k.shortener.config.CreateShortUrlDto
+import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.awaitBody
+
+suspend fun handleCreateShortUrl(serverRequest: ServerRequest): ServerResponse = serverRequest
+    .awaitBody<CreateShortUrlDto>()
+    .let {
+        ServerResponse
+            .ok()
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(it)
+            .awaitSingle()
+    }
+
