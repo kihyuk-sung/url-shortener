038ff826c930959581ac8f50c4f6bf14c1d7b79e
K <kihyuk.sung@outlook.kr>
2023-02-25 16:16:04 +0900

    feat: base62ToUUID

--- src/main/kotlin/org/k/shortener/domain/id/IdUtils.kt
+++ src/main/kotlin/org/k/shortener/domain/id/IdUtils.kt
@@ -100,3 +100,89 @@ private val base62DigitMap = charArrayOf(
 )
 
 private fun Int.toBase62Digit(): Char = base62DigitMap.getOrNull(this) ?: throw AssertionError()
+
+private val base62Digit = mapOf(
+    'A' to (0).toBigInteger(),
+    'B' to (1).toBigInteger(),
+    'C' to (2).toBigInteger(),
+    'D' to (3).toBigInteger(),
+    'E' to (4).toBigInteger(),
+    'F' to (5).toBigInteger(),
+    'G' to (6).toBigInteger(),
+    'H' to (7).toBigInteger(),
+    'I' to (8).toBigInteger(),
+    'J' to (9).toBigInteger(),
+    'K' to (10).toBigInteger(),
+    'L' to (11).toBigInteger(),
+    'M' to (12).toBigInteger(),
+    'N' to (13).toBigInteger(),
+    'O' to (14).toBigInteger(),
+    'P' to (15).toBigInteger(),
+    'Q' to (16).toBigInteger(),
+    'R' to (17).toBigInteger(),
+    'S' to (18).toBigInteger(),
+    'T' to (19).toBigInteger(),
+    'U' to (20).toBigInteger(),
+    'V' to (21).toBigInteger(),
+    'W' to (22).toBigInteger(),
+    'X' to (23).toBigInteger(),
+    'Y' to (24).toBigInteger(),
+    'Z' to (25).toBigInteger(),
+    'a' to (26).toBigInteger(),
+    'b' to (27).toBigInteger(),
+    'c' to (28).toBigInteger(),
+    'd' to (29).toBigInteger(),
+    'e' to (30).toBigInteger(),
+    'f' to (31).toBigInteger(),
+    'g' to (32).toBigInteger(),
+    'h' to (33).toBigInteger(),
+    'i' to (34).toBigInteger(),
+    'j' to (35).toBigInteger(),
+    'k' to (36).toBigInteger(),
+    'l' to (37).toBigInteger(),
+    'm' to (38).toBigInteger(),
+    'n' to (39).toBigInteger(),
+    'o' to (40).toBigInteger(),
+    'p' to (41).toBigInteger(),
+    'q' to (42).toBigInteger(),
+    'r' to (43).toBigInteger(),
+    's' to (44).toBigInteger(),
+    't' to (45).toBigInteger(),
+    'u' to (46).toBigInteger(),
+    'v' to (47).toBigInteger(),
+    'w' to (48).toBigInteger(),
+    'x' to (49).toBigInteger(),
+    'y' to (50).toBigInteger(),
+    'z' to (51).toBigInteger(),
+    '0' to (52).toBigInteger(),
+    '1' to (53).toBigInteger(),
+    '2' to (54).toBigInteger(),
+    '3' to (55).toBigInteger(),
+    '4' to (56).toBigInteger(),
+    '5' to (57).toBigInteger(),
+    '6' to (58).toBigInteger(),
+    '7' to (59).toBigInteger(),
+    '8' to (60).toBigInteger(),
+    '9' to (61).toBigInteger(),
+)
+
+fun String.base62ToUUID(): UUID = this
+    .toBigInteger()
+    .toByteArray()
+    .run {
+        val upper = ByteArray(8)
+        val lower = ByteArray(8)
+        for (i in 0 until 8) {
+            lower[7 - i] = this.getOrElse(i) { 0 }
+        }
+        for (i in 8 until 16) {
+            upper[15 - i] = this.getOrElse(i) { 0 }
+        }
+        upper to lower
+    }
+    .let { (upper, lower) -> ByteBuffer.wrap(upper) to ByteBuffer.wrap(lower) }
+    .let { (upper, lower) -> upper.long to lower.long }
+    .let { (upper, lower) -> UUID(upper, lower) }
+private fun String.toBigInteger(): BigInteger = fold(BigInteger.ZERO) { acc, c ->
+    acc * BIGINT_62 + (base62Digit[c] ?: throw NumberFormatException())
+}
--- src/test/kotlin/org/k/shortener/domain/id/IdUtilsKtTest.kt
+++ src/test/kotlin/org/k/shortener/domain/id/IdUtilsKtTest.kt
@@ -3,6 +3,7 @@ package org.k.shortener.domain.id
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import java.math.BigInteger
+import java.nio.ByteBuffer
 import java.util.UUID
 
 class IdUtilsKtTest : FunSpec({
@@ -15,5 +16,38 @@ class IdUtilsKtTest : FunSpec({
     test("biginteger to base62") {
         BigInteger.ZERO.toBase62() shouldBe "A"
         BigInteger.valueOf(62).toBase62() shouldBe "BA"
+    }
+
+    test("bytebuffer to long") {
+        ByteBuffer.wrap(byteArrayOf(0, 0, 0, 0, 0, 0, 0, 0)).long shouldBe 0L
+        ByteBuffer.wrap(byteArrayOf(0, 0, 0, 0, 0, 0, 0, 1)).long shouldBe 1L
+    }
+
+    test("base62 to UUID") {
+        "A".base62ToUUID() shouldBe UUID(0, 0)
+        "B".base62ToUUID() shouldBe UUID(0, 1)
+        "C".base62ToUUID() shouldBe UUID(0, 2)
+        "D".base62ToUUID() shouldBe UUID(0, 3)
+        "E".base62ToUUID() shouldBe UUID(0, 4)
+        "F".base62ToUUID() shouldBe UUID(0, 5)
+        "G".base62ToUUID() shouldBe UUID(0, 6)
+        "H".base62ToUUID() shouldBe UUID(0, 7)
+        "I".base62ToUUID() shouldBe UUID(0, 8)
+        "J".base62ToUUID() shouldBe UUID(0, 9)
+        "K".base62ToUUID() shouldBe UUID(0, 10)
+        "L".base62ToUUID() shouldBe UUID(0, 11)
+        "M".base62ToUUID() shouldBe UUID(0, 12)
+        "N".base62ToUUID() shouldBe UUID(0, 13)
+        "O".base62ToUUID() shouldBe UUID(0, 14)
+        "P".base62ToUUID() shouldBe UUID(0, 15)
+        "Q".base62ToUUID() shouldBe UUID(0, 16)
+        "R".base62ToUUID() shouldBe UUID(0, 17)
+        "S".base62ToUUID() shouldBe UUID(0, 18)
+        "T".base62ToUUID() shouldBe UUID(0, 19)
+        "U".base62ToUUID() shouldBe UUID(0, 20)
+        "V".base62ToUUID() shouldBe UUID(0, 21)
+        "9".base62ToUUID() shouldBe UUID(0, 61)
+        "BA".base62ToUUID() shouldBe UUID(0, 62)
+        "BB".base62ToUUID() shouldBe UUID(0, 63)
     }
 })
