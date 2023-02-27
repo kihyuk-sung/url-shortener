c107715c4566085fbfff62376a51839a69714300
K <kihyuk.sung@outlook.kr>
2023-02-27 18:45:39 +0900

    feat: createShortUrl
    - uuid 랜덤 생성
    - ShortUrlDto 로 변경
    - 해당 객체의 shortUrl 반환

--- src/main/kotlin/org/k/shortener/domain/UrlShortener.kt
+++ src/main/kotlin/org/k/shortener/domain/UrlShortener.kt
@@ -1,3 +1,15 @@
 package org.k.shortener.domain
 
-fun createShortUrl(url: String): String = url
+import org.k.shortener.domain.id.toBase62
+import java.util.UUID
+
+fun createShortUrl(url: String): String = UUID
+    .randomUUID()
+    .let {
+        ShortUrlDto(
+            id = it,
+            shortUrl = it.toBase62(),
+            url = url,
+        )
+    }
+    .shortUrl
