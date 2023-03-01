5bed8ab173e9887296409a4dee4c0ccdc37c8b4e
K <kihyuk.sung@outlook.kr>
2023-03-01 11:41:02 +0900

    feat: 임시 url 추가
    - POST url: 데이터 임시 저장
    - GET url: 저장한 데이터 전부 조회
    - mariadb driver로 mysql 연결 확인

--- src/main/kotlin/org/k/shortener/config/RouterConfiguration.kt
+++ src/main/kotlin/org/k/shortener/config/RouterConfiguration.kt
@@ -1,19 +1,46 @@
 package org.k.shortener.config
 
-import org.k.shortener.handler.handleCreateShortUrl
+import kotlinx.coroutines.reactor.awaitSingle
+import org.k.shortener.domain.ShortUrlDto
+import org.k.shortener.domain.id.toBase62
 import org.k.shortener.handler.makeShortUrlHandler
+import org.k.shortener.persistant.ShortUrlAdapter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.bodyAndAwait
 import org.springframework.web.reactive.function.server.coRouter
+import java.util.*
 
 @Configuration
-class RouterConfiguration {
+class RouterConfiguration(
+    private val shortUrlAdapter: ShortUrlAdapter,
+) {
     @Bean("mainRouter")
     fun mainRouter() = coRouter {
-        GET("/{shortUrl}", makeShortUrlHandler("shortUrl"))
         accept(MediaType.APPLICATION_JSON).nest {
-            POST("/url", ::handleCreateShortUrl)
+            POST("/url") {
+                val id = UUID.randomUUID()
+                val body = shortUrlAdapter.save(
+                    ShortUrlDto(
+                        id = id,
+                        shortUrl = id.toBase62(),
+                        url = id.toBase62(),
+                    )
+                )
+                ServerResponse
+                    .ok()
+                    .bodyValue(body)
+                    .awaitSingle()
+            }
+
+            GET("/url") {
+                ServerResponse
+                    .ok()
+                    .bodyAndAwait(shortUrlAdapter.findAll())
+            }
         }
+        GET("/{shortUrl}", makeShortUrlHandler("shortUrl"))
     }
 }
